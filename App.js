c874495946aa4c3b2af945a4be23aa844f56d006
Guangzhong Chen <guangzhong0@gmail.com>
2022-07-27 18:32:42 +0100

    Added Initial Bottom Navigation bar

--- App.js
+++ App.js
@@ -1,12 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View, Button, TextInput, TabBarIOSItem} from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+function EventScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Event!</Text>
+    </View>
+    // <View style={styles.container}>
+    //   <Text style={styles.red_box}>
+    //     Open up App.js to start working on your app!
+    //   </Text>
+    //   <Button title='Click Me'/>
+    //   <TextInput></TextInput>
+    //   <StatusBar style="auto" />
+    //   <View>
+    //     <Tab.Navigator>
+    //       <Tab.Screen name="Home" component={HomeScreen} />
+    //       <Tab.Screen name="Settings" component={SettingsScreen} />
+    //     </Tab.Navigator>
+    //   </View>
+    // </View>
+  );
+}
+
+function NearbyScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function ChatScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Chat!</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={EventScreen} />
+        <Tab.Screen name="Settings" component={NearbyScreen} />
+        <Tab.Screen name="Chat" component={ChatScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +71,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  red_box: {
+    borderWidth: 2,
+    borderColor: '#ff0000',
+    padding: 16
+  }
 });
