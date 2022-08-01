210af06ea99321404da1c0e0e46bced0df55dd32
Andrew <andrewtam0077@gmail.com>
2022-07-31 22:28:20 -0400

    Finished dummy signIn Screen

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
-import {View, Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
+import {View, Text, Image, StyleSheet, useWindowDimensions, ScrollView} from 'react-native';
+import Logo from '../../assets/imgOne.png';
 import CustomInput from '../components/CustomInput';
+import CustomButton from '../components/CustomButton';
 
 const SignInScreen = () => {
     const [username, setUsername] = useState('');
@@ -8,28 +10,68 @@ const SignInScreen = () => {
 
     const {height} = useWindowDimensions();
 
+    const onSignInPressed = () => {
+        console.warn("Sign In");
+    }
+
+    const onForgotPasswordPressed = () => {
+        console.warn('Forgot password pressed')
+    }
+
+    const onNoAccount = () => {
+        console.warn('User needs to create an account')
+    }
+
     return (
+        <ScrollView>
+
         <View style={styles.root}> 
-        <CustomInput 
-            placeholder="Username"
-            value={username} 
-            setValue={setUsername}
-        />
-        <CustomInput 
-            placeholder="Password" 
-            value={password} 
-            setValue={setPassword}
-            secureTextEntry={true}
-        />
+            <Image 
+                source={Logo}
+                style={[styles.logo, {height: height * 0.3}]}
+                resizeMode="contain"
+            />
+            <CustomInput 
+                placeholder="Username"
+                value={username} 
+                setValue={setUsername}
+            />
+            <CustomInput 
+                placeholder="Password" 
+                value={password} 
+                setValue={setPassword}
+                secureTextEntry={true}
+            />
+            <CustomButton 
+                text="Log In" 
+                onPress={onSignInPressed} 
+                type='PRIMARY'
+            />
+            <CustomButton 
+                text="Forgot password?" 
+                onPress={onForgotPasswordPressed} 
+                type='TERTIARY' 
+            />
+            <CustomButton
+                text="Don't have an account?"
+                onPress={onSignInPressed}
+                type='TERTIARY'
+            />
 
         </View>
+        </ScrollView>
     );
 }
 const styles = StyleSheet.create({
     root: {
         alignItems: 'center',
         padding: 20,
-    }
+    },
+    logo: {
+        width: '100%',
+        maxWidth: 200,
+        maxHeight: 200,
+    } 
 })
 
 export default SignInScreen
