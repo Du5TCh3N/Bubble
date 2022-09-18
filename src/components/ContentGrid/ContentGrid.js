3592d3e2e852dfe0961cb2c8e75b2368358fd9a6
Guangzhong Chen <guangzhong0@gmail.com>
2022-09-18 22:35:44 +0100

    Migrated changes from feat/bubble-grid to feat/content-page

---
+++ src/components/ContentGrid/ContentGrid.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import { View, FlatList, Text, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView} from 'react-native';
+import { Card, Title, Paragraph} from 'react-native-paper';
+import { FlatGrid } from 'react-native-super-grid';
+
+import SearchBar from "../../components/SearchBar";
+
+const bubbleList = [
+    {key: '1', name: 'Come Drink!', img: 'https://picsum.photos/700', description: "lorem piscum"},
+    {key: '2', name: 'Come Drink 1', img: 'https://picsum.photos/700', description: "lorem piscum"},
+    {key: '3', name: 'Come Drink 2', img: 'https://picsum.photos/700', description: "lorem piscum"},
+    {key: '4', name: 'Come Drink 3', img: 'https://picsum.photos/700', description: "lorem piscum"},
+    {key: '5', name: 'Come Drink 4', img: 'https://picsum.photos/700', description: "lorem piscum"},
+    {key: '6', name: 'Come Drink 5', img: 'https://picsum.photos/700', description: "lorem piscum"},
+]
+
+function ContentGrid() {
+
+    const getHeader = () => {
+        return (
+            <View>
+                <SearchBar></SearchBar>
+            </View>
+            
+        )
+    }
+    
+      return (
+            <FlatGrid
+                itemDimension={150}
+                data={bubbleList}
+                style={styles.gridView}
+                spacing={2}
+                renderItem={({ item }) => (
+                    <TouchableOpacity>
+                        <Card>
+                            <Card.Cover source={{ uri: item.img }} />
+                            <Card.Title title= {item.name} subtitle= {item.code}/>
+                            <Card.Content>
+                                <Paragraph>{item.description}</Paragraph>
+                            </Card.Content>
+                        </Card>
+                    </TouchableOpacity>
+                )}
+                ListHeaderComponent={getHeader}
+            />
+        
+      );
+}   
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    gridView: {
+        marginTop: 0,
+        flex: 1,
+        width: '100%',
+    },
+    redBorder: {
+        borderColor: "red",
+        borderWidth: 4,
+    }
+});
+
+export default ContentGrid
--- src/screens/BubbleScreen/BubbleScreen.js
+++ src/screens/BubbleScreen/BubbleScreen.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList, ScrollView, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
+import { Card, Title, Paragraph} from 'react-native-paper';
+import { FlatGrid } from 'react-native-super-grid';
+
+/**
+ * Import components
+ */
+import ContentGrid from "../../components/ContentGrid";
+import SearchBar from "../../components/SearchBar";
+
+const bubbleList = [
+  {key: '1', name: 'Come Drink!', img: 'https://picsum.photos/700', description: "lorem piscum"},
+  {key: '2', name: 'Come Drink1', img: 'https://picsum.photos/700', description: "lorem piscum"},
+  {key: '3', name: 'Come Drink2', img: 'https://picsum.photos/700', description: "lorem piscum"},
+  {key: '4', name: 'Come Drink3', img: 'https://picsum.photos/700', description: "lorem piscum"},
+  {key: '5', name: 'Come Drink4', img: 'https://picsum.photos/700', description: "lorem piscum"},
+  {key: '6', name: 'Come Drink5', img: 'https://picsum.photos/700', description: "lorem piscum"},
+]
 
 function BubbleScreen() {
+
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        height: 100,
-        padding: 20
-      }}
-    >
-      <Text>Bubble Screen</Text>
-    </View>
+      <ContentGrid></ContentGrid>
+      
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+  },
+  gridView: {
+      marginTop: 0,
+      flex: 1,
+  },
+});
 
 export default BubbleScreen;
