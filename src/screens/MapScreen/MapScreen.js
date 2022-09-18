87eefe5feaf48ce5f7bab0f60d37007910c6bfd6
tanyuzhuo <37593168+tanyuzhuo@users.noreply.github.com>
2022-09-18 23:26:42 +0100

    Init MapView (#15)

--- src/screens/MapScreen/MapScreen.js
+++ src/screens/MapScreen/MapScreen.js
@@ -1,18 +1,35 @@
-import React from "react";
-import { View, Text } from "react-native";
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 function MapScreen() {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        height: 100,
-        padding: 20
-      }}
-    >
-      <Text>Map Screen</Text>
+    <View style={styles.container}>
+      <MapView 
+        style={styles.map} 
+        //specify our coordinates.
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}   
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
 export default MapScreen;
