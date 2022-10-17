288a024116ec65e8bceda1b6babb705c6a27a465
zbw790 <52985032+zbw790@users.noreply.github.com>
2022-10-17 12:45:16 +0100

    some edit on map (#30)
    Co-authored-by: zbw790 <k21083430@kcl.ac.uk>

--- src/screens/MapScreen/MapScreen.js
+++ src/screens/MapScreen/MapScreen.js
@@ -1,20 +1,115 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Callout, Circle, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { Provider } from 'react-native-paper';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import Constants from "expo-constants";
+import PropTypes from "prop-types";
 
 function MapScreen() {
+  const [ pin, setPin ] = React.useState({
+    latitude: 51.51758,
+    longitude: -0.11783, 
+  })
+  const [ region, setRegion ] = React.useState({
+    latitude: 51.51758,
+    longitude: -0.11783, 
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  })
   return (
     <View style={styles.container}>
       <MapView 
-        style={styles.map} 
+        style={styles.map}
+        provider={PROVIDER_GOOGLE} 
         //specify our coordinates.
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 51.51758,
+          longitude: -0.11783,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}   
-      />
+      >
+        <Marker coordinate={{latitude: region.latitude, longitude: region.longitude}}/>
+        <Marker 
+          coordinate = {pin}
+          draggable = {true}
+          onDragStart = {(e) => {
+            console.log("Drag start", e.nativeEvent.coordinate)
+          }}
+          onDragEnd = {(e) => {
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            })
+          }}
+        >
+            <Callout>
+              <Text>I'm here!</Text>
+            </Callout>
+        </Marker>
+        <Circle 
+          center={pin}
+          radius = {1500}
+        />
+      </MapView>
+
+      <View style={styles.searchContainer}>
+        <Text>From</Text>
+        <GooglePlacesAutocomplete
+          style={{ textInput: styles.input}}
+          placeholder='Search'
+          fetchDetails= {true}
+          GooglePlacesSearchQuery = {{
+            rankby: "distance",
+          }}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+            setRegion({
+              latitude: details.geometry.location.lat,
+              latitude: details.geometry.location.lng,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            })
+          }}
+          query={{
+            key: 'AIzaSyDbxFPw8cVHVOhLZumDDLZF7oA9H6l5br0',
+            language: 'en',
+            components: "country:uk",
+            types: "establishment",
+            radius: 30000,
+            location: `${region.latitude}, ${region.longitude}`
+          }}
+        />
+        <Text>To</Text>
+        <GooglePlacesAutocomplete
+          style={{ textInput: styles.input}}
+          placeholder='Search'
+          fetchDetails = {true}
+          GooglePlacesSearchQuery = {{
+            rankby: "distance",
+          }}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+            setRegion({
+              latitude: details.geometry.location.lat,
+              latitude: details.geometry.location.lng,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            })
+          }}
+          query={{
+            key: 'AIzaSyDbxFPw8cVHVOhLZumDDLZF7oA9H6l5br0',
+            language: 'en',
+            components: "country:uk",
+            types: "establishment",
+            radius: 30000,
+            location: `${region.latitude}, ${region.longitude}`
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -29,6 +124,23 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+  searchContainer: {
+    position: "absolute",
+    width: "90%",
+    backgroundColor: "white",
+    shadowColor: "black",
+    textShadowOffset: {width: 2, height:2},
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 4,
+    padding: 8,
+    borderRadius: 8,
+    top: Constants.statusBarHeight,
+  },
+  input: {
+    borderColor: "#888",
+    borderWidth: 1,
   },
 });
 
